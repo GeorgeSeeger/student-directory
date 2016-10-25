5397ed45a16ee049dbb413dfbde808cb43e00fc1
George Seeger <georgeseeger@hotmail.com>
2016-10-25 12:23:15 +0100

    directory.rb refactored to use instance variable @ students and code cleaned up

--- directory.rb
+++ directory.rb
@@ -1,16 +1,5 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort:  :november},
-  {name: "Darth Vader", cohort:  :november},
-  {name: "Nurse Ratched", cohort:  :november},
-  {name: "Michael Corleone", cohort:  :november},
-  {name: "Alex DeLarge", cohort:  :november},
-  {name: "The Wicked Witch of the West", cohort:  :november},
-  {name: "Terminator", cohort:  :november},
-  {name: "Freddy Krueger", cohort:  :november},
-  {name: "The Joker", cohort:  :november},
-  {name: "Joffrey Baratheon", cohort:  :november},
-  {name: "Norman Bates", cohort:  :november}
-]
+@students = []
+
 def print_header
   puts "The students of Villains Academy"
   puts "-"*10
@@ -25,11 +14,11 @@ def print_footer(names)
   puts "Overall we have #{names.count} great students"
 end
 
-def print_only_specific(students)
+def print_only_specific
   print "Filter for names beginning with: "
   starting_char = gets.chomp.upcase
   puts "The only students with name beginning with #{starting_char} are:"
-  students.each{ |e|
+  @students.each{ |e|
     if e[:name][0].upcase == starting_char then
       puts "#{e[:name]}"
     end
@@ -39,20 +28,17 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, hit return twice"
-  #create empty array
-  students = []
   #get names from user
   print "Enter a name: "; name = gets.chomp
   print "Enter a cohort: "; cohort = get_cohort
   #until double return
   while name != ""
-    students.push({name: name, cohort: cohort})
-    puts "Now we have #{students.count} students"
+    @students.push({name: name, cohort: cohort})
+    puts "Now we have #{@students.count} students"
     #get another name
     print "Enter a name: "; name = gets.chomp
     if !name.empty? then  print "Enter a cohort: "; cohort = get_cohort end
   end
-  students
 end
 
 
@@ -65,32 +51,41 @@ def get_cohort
    cohort
 end
 
-def interactive_menu
-  students = []
+def print_menu
   puts "Please select an option, eg 1"
   puts "\t 1. Input student data"
   puts "\t 2. Display all students"
   puts "\t 3. Filter students buy first letter of name"
   puts "\t 9. Exit program"
+end
 
-  #selection = gets.chomp
+def show_students
+  print_header
+  print_students(@students)
+  print_footer(@students)
+end
+
+def process
+  print "What is your selection? "
+  selection = gets.chomp
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    print_only_specific
+  when "9"
+    exit
+  else
+    puts "I don't understand, please enter a number"
+  end
+end
+
+def interactive_menu
   loop do
-    print "What is your selection? "
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print_students(students)
-      print_footer(students)
-    when "3"
-      print_only_specific(students)
-    when "9"
-      exit
-    else
-      puts "I don't understand, please enter a number"
-    end
+    print_menu
+    process
   end
 end
 
