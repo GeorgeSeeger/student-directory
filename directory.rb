e897cd9ee8f4d143e993d34e5625aba817e886ce
George Seeger <georgeseeger@hotmail.com>
2016-10-25 12:16:31 +0100

    Exercise 9 - interactive menu - completed

--- directory.rb
+++ directory.rb
@@ -28,10 +28,9 @@ end
 def print_only_specific(students)
   print "Filter for names beginning with: "
   starting_char = gets.chomp.upcase
-  puts "The only students with name beginning with #{starting_char}
-  and length < 12  are:"
+  puts "The only students with name beginning with #{starting_char} are:"
   students.each{ |e|
-    if e[:name][0].upcase == starting_char && e[:name].length < 12 then
+    if e[:name][0].upcase == starting_char then
       puts "#{e[:name]}"
     end
   }
@@ -48,10 +47,10 @@ def input_students
   #until double return
   while name != ""
     students.push({name: name, cohort: cohort})
-    puts "No we have #{students.count} students"
-  #get another name
-  print "Enter a name: "; name = gets.chomp
-  print "Enter a cohort: "; cohort = get_cohort
+    puts "Now we have #{students.count} students"
+    #get another name
+    print "Enter a name: "; name = gets.chomp
+    if !name.empty? then  print "Enter a cohort: "; cohort = get_cohort end
   end
   students
 end
@@ -66,8 +65,33 @@ def get_cohort
    cohort
 end
 
-students = input_students
-print_header
-print_students(students)
-#print_only_specific(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  puts "Please select an option, eg 1"
+  puts "\t 1. Input student data"
+  puts "\t 2. Display all students"
+  puts "\t 3. Filter students buy first letter of name"
+  puts "\t 9. Exit program"
+
+  #selection = gets.chomp
+  loop do
+    print "What is your selection? "
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print_students(students)
+      print_footer(students)
+    when "3"
+      print_only_specific(students)
+    when "9"
+      exit
+    else
+      puts "I don't understand, please enter a number"
+    end
+  end
+end
+
+interactive_menu
